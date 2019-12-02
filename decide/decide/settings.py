60fab8bb8850f5e8028acdbcd3bee67f83c9e6b8
Alfonso <2aalfonsoromero@gmail.com>
2019-12-02 11:51:14 +0100

    arreglado settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://decide-alf-practica7.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
